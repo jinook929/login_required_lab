71b5a152e4a5f038bb9ced78b2c13ce795756289
jinook929 <jinook929@gmail.com>
2021-03-22 22:50:50 -0400

    commit tree f65509ee578bd03a3cf020c0c55fca0d6ad1bc3b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'secret', to: 'secrets#show'
+
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  get 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
